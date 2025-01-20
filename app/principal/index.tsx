0e16a17b53adc5f05ba5c845944925add8da1821
Myriam <5525235@alu.murciaeduca.es>
2025-01-20 20:49:37 +0100

    subida de prueba colaborador

--- app/principal/index.tsx
+++ app/principal/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { GlobalStyles } from "@/src/theme/GlobalStyles";
 
 export default function Index() {
   return (
     <View style={GlobalStyles.container}>
-      <Text style={GlobalStyles.titulo}>Inicio</Text>
+        <View>
+          <Image source={require('../../assets/images/fotoperfil.png')} style={GlobalStyles.imagenPerfil}/>
+        </View>
     </View>
+
+
   );
 }
--- src/theme/GlobalStyles.tsx
+++ src/theme/GlobalStyles.tsx
@@ -4,7 +4,6 @@ import { Tamanos } from './Tamanos';
 
 export const GlobalStyles = StyleSheet.create({
     container: {
-        backgroundColor: Colores.colorFondo,
         minHeight: '100%',
         flex: 1,
         flexDirection: 'column',
@@ -24,71 +23,10 @@ export const GlobalStyles = StyleSheet.create({
         backgroundColor: Colores.colorFondoTarjeta,
         fontWeight: "bold"
     },
-    peliculaPreview: {
-        padding: 10,
-        margin: 10,
-        borderRadius: 10,
-        borderColor: Colores.colorBordePreview,
-        borderWidth: 3,
-        backgroundColor: Colores.colorFondoTarjeta,
-    },
-    titulo: {
-        fontFamily: 'BebasNeue',
-        color: Colores.colorTitulo,
-        fontSize: Tamanos.textoMediano,
-        fontWeight: Tamanos.weightGrande,
-        textAlign: 'center',
-        paddingBottom: 10
-    },
-    enlace: {
-        color: Colores.colorEnlace,
-        textDecorationLine: 'underline',
-        paddingTop: 10,
-    },
-    fotoPeli: {
+    imagenPerfil: {
         width: Tamanos.anchoFoto,
         height: Tamanos.alturaFoto,
-        marginTop: 15,
-        alignSelf: 'center',
         borderRadius: 10,
-        borderColor: Colores.colorBordePreview,
-        borderWidth: 2,
     },
-    textoDefault: {
-        color: Colores.colorTextoPantalla,
-        fontSize: Tamanos.textoChico,
-        fontFamily: 'RobotoSlab',
-    },
-    nota: {
-        fontFamily: 'BebasNeue',
-        fontSize: Tamanos.textoMediano,
-        fontWeight: Tamanos.weightGrande,
-        textAlign: 'center',
-        paddingBottom: 10
-    },
-    notaBuena: {
-        color: Colores.notaBuena,
-    },
-    notaMedia: {
-        color: Colores.notaMedia
-    },
-    notaMala: {
-        color: Colores.notaMala
-    },
-    boton: {
-        backgroundColor: Colores.colorBoton,
-        padding: 10,
-        borderRadius: 10,
-        borderColor: Colores.colorBordeBoton,
-        borderWidth: 2,
-        textAlign: 'center',
-        width: 200,
-        alignSelf: 'center',
-        marginTop: 10,
-    },
-    logo: {
-        width: 200,
-        height: 200,
-        alignSelf: 'center',
-    }
+    
 });
