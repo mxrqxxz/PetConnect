446216555ec16a7315d3360764c580840446f6cf
mxrqxxz <4186790@alu.murciaeduca.es>
2025-01-29 02:37:42 +0100

    Se ha bloqueado el acceso a sobre nosotros y a patitas en marcha empleando redirecciones

--- app/drawer/nosotros/index.tsx
+++ app/drawer/nosotros/index.tsx
@@ -1,7 +1,16 @@
 import { Text, View } from "react-native";
 import { GlobalStyles } from "@/src/theme/GlobalStyles";
+import { useRouter } from "expo-router";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback } from "react";
 
 export default function Index() {
+  const router = useRouter();
+  useFocusEffect(
+    useCallback(() => {
+      router.replace("/drawer");
+    }, [])
+  );
   return (
     <View style={GlobalStyles.container}>
       <Text>Sobre Nosotros</Text>
